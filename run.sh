30b0d1d039f2ac39303eb5864c800d72aa72ba56
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2015-04-02 17:13:29 +0900

    remove sshd add init scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+echo "installing" > /var/log/container_status
+
+echo "Running init script"
+bash /tmp/init/init.sh
+
 echo "complete" > /var/log/container_status
 
 /usr/bin/supervisord
