54ff2d77daf62b7d49c4669a23ffff4cdeba3d66
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2016-01-15 15:11:13 +0900

    add nginx env var to configure document root

--- run.sh
+++ run.sh
@@ -3,6 +3,13 @@
 
 echo "installing" > /var/log/container_status
 
+# setup webserver doc roots
+DOCROOT=`echo $WEBSERVER_DOCUMENT_ROOT | sed 's/[]\/$*.^|[]/\\&/g'`
+sed -i -- "s/##WEBSERVER_DOCUMENT_ROOT##/$DOCROOT/g" /etc/nginx/sites-available/default
+
+# Clean double slashes
+sed -i -- 's/www\/\//www\//g' /etc/nginx/sites-available/default
+
 echo "Running init script"
 bash /tmp/init/init.sh
 
