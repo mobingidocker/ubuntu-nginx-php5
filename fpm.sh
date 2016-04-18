8c057d36f7845a2ec6b6c443f29549a6fac5f919
David Siaw <davidsiaw@gmail.com>
2016-04-18 16:59:57 +0900

    fix problem that arises when an equals sign appears in the env var

--- fpm.sh
+++ fpm.sh
@@ -3,5 +3,5 @@
 chown -R www-data /usr/share/nginx/www
 chgrp -R www-data /usr/share/nginx/www
 
-env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" > /usr/local/etc/conf.d/env.conf
+env | sed "s/\([^=]*\)=\(.*\)$/env[\1]='\2'/" > /usr/local/etc/conf.d/env.conf
 /usr/sbin/php5-fpm -c /etc/php5/fpm
